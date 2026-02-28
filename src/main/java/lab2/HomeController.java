6e0f73eb4b612dc80da19ef37a79aa3d4e05341b
Star <rubiyasultana0@gmail.com>
2026-02-28 03:33:06 -0500

    initial spring boot project

---
+++ src/main/java/lab2/HomeController.java
@@ -0,0 +1,14 @@
+package lab2;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to COMP367";
+    }
+}
